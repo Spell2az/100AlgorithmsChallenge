76fc75afa13f3cdac26dad1292999d7637ba07a8
Spell2az <martin_kovac@outlook.com>
2018-09-09 09:36:25 +0100

    Solve extractEactKth

---
+++ solvedAlgorithms/extractEachKth/extractEachKth.js
@@ -0,0 +1,4 @@
+function extractEachKth(inputArray, k) {
+    return inputArray.filter(function (el, idx) { return (idx + 1) % k !== 0; });
+}
+console.log(extractEachKth([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
