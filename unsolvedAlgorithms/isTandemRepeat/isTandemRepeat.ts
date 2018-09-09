80618ff5847bd8f96063324e61651d1b95325b02
Martin Kovac <martin_kovac@outlook.com>
2018-09-09 12:24:43 +0100

    Remove solved

--- unsolvedAlgorithms/isTandemRepeat/isTandemRepeat.ts
+++
@@ -1,9 +0,0 @@
-function isTandemRepeat(inputString: string): boolean {
-  if (inputString.length % 2 !== 0) return false;
-
-  return inputString.slice(0, inputString.length / 2) === inputString.slice(inputString.length / 2)
-}
-
-console.log(isTandemRepeat('tandemtandem'))
-console.log(isTandemRepeat('qqq'))
-console.log(isTandemRepeat('2w2ww'))
--- unsolvedAlgorithms/shapeArea/shapeArea.js
+++
@@ -1,8 +0,0 @@
-function shapeArea(n) {
-    if (n === 0) {
-        return 1;
-    }
-    return 4 * (n - 1) + shapeArea(n - 1);
-}
-console.log(shapeArea(2));
-console.log(shapeArea(3));
