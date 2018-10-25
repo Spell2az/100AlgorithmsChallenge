57e413d0c767e8e231a16c184f6a0821665529c6
Martin Kovac <martin_kovac@outlook.com>
2018-10-25 20:55:52 +0100

    Removed files

--- unsolvedAlgorithms/htmlEndTagByStartTag/htmlEndTagByStartTag.js
+++
@@ -1,5 +0,0 @@
-function htmlEndTagByStartTag(startTag) {
-    return "</" + startTag.match(/[a-z]+/g)[0] + ">";
-}
-console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
-console.log(htmlEndTagByStartTag('<i>'));
--- unsolvedAlgorithms/htmlEndTagByStartTag/htmlEndTagByStartTag.ts
+++
@@ -1,6 +0,0 @@
-function htmlEndTagByStartTag(startTag: string): string {
-  return `</${startTag.match(/[a-z]+/g)[0]}>`;
-}
-
-console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
-console.log(htmlEndTagByStartTag('<i>'));
