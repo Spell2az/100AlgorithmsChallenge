285ad1097050c83c85a248dbe30982982b9c2c7d
Spell2az <[omitted]>
2018-11-05 17:15:15 +0000

    Solve containsDuplicates

---
+++ solvedAlgorithms/containsDuplicates/containsDuplicates.js
@@ -0,0 +1,6 @@
+function containsDuplicates(a) {
+    var set = new Set(a);
+    return a.length !== set.size;
+}
+console.log(containsDuplicates([1, 2, 3, 1]));
+console.log(containsDuplicates([3, 1]));
--- unsolvedAlgorithms/avoidObstacles/avoidObstacles.ts
+++
@@ -1,7 +0,0 @@
-function avoidObstacles(inputArray: number[]): number {
-  for (let i: number = 2; ; i++) {
-    if (inputArray.every(el => el % i !== 0)) { return i; }
-  }
-}
-
-console.log(avoidObstacles([5, 3, 6, 7, 9]));
