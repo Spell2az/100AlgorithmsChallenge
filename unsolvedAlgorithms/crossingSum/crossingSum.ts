4e2a709632ca06a82c9ff1717debfe70d4159cc7
Spell2az <martin_kovac@outlook.com>
2018-11-14 23:45:58 +0000

    remove solved

--- unsolvedAlgorithms/crossingSum/crossingSum.ts
+++ unsolvedAlgorithms/crossingSum/crossingSum.ts
@@ -1,7 +1,13 @@
 function crossingSum(matrix: number[][], a: number, b: number): number {
-  
+  const column = matrix
+    .reduce((acc, curr) => {
+      return [...acc, curr[b]];
+    }, [])
+    .filter((_, idx) => idx !== a);
+
+  const row = matrix[b];
+
+  return column.concat(row).reduce((acc, cur) => acc + cur, 0);
 }
 
-console.log(crossingSum([[1, 1, 1, 1], 
-    [2, 2, 2, 2], 
-    [3, 3, 3, 3]], 1, 3));
+console.log(crossingSum([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3]], 1, 3));
